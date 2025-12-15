078126b50de9ebbc48dbac4c7582cc47e489425b
Ahickling04 <ianhickling0@gmail.com>
2025-12-15 14:11:02 +0000

    fixed formatting and buttons

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ let contactInfo = document.getElementById("contactInfo");
 let projects = document.getElementById("projects");
 let osmp = document.getElementById("osmp");
 let ttt = document.getElementById("ticTacTUI");
+let github = document.getElementById("github");
+let linkedin = document.getElementById("linkedin");
+
+github.addEventListener("click", () => {
+    open("https://github.com/Ahickling04")
+})
+
+linkedin.addEventListener("click", () => {
+    open("https://www.linkedin.com/in/ahickling04/")
+})
 
 aboutMe.addEventListener("click", () => {
     window.location.href = "about.html";
