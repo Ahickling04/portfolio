c13a2c4f8d24046dbde6d86eb9537b314b0787ad
Ahickling04 <ianhickling0@gmail.com>
2026-01-02 18:23:02 +0000

    added content to project page as well as its own css file

--- projects.js
+++ projects.js
@@ -1,5 +1,15 @@
 let homeButton = document.getElementById('homeButton');
+let osmp = document.getElementById("osmp");
+let ttt = document.getElementById("ticTacTUI");
 
 homeButton.addEventListener('click', function() {
     window.location.href = 'index.html';
 });
+
+osmp.addEventListener("click", () => {
+    open("https://github.com/Ahickling04/osmp")
+})
+
+ttt.addEventListener("click", () => {
+    open("https://github.com/Ahickling04/tic-tac-tui")
+})
