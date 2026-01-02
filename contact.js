0f53af8d365d95059e16be42b52d94b42199c2c3
Ahickling04 <ianhickling0@gmail.com>
2026-01-02 17:35:34 +0000

    added content to contact as well as its own css file

--- contact.js
+++ contact.js
@@ -1,4 +1,19 @@
 let homeButton = document.getElementById('homeButton');
+let github = document.getElementById('github');
+let linkedin = document.getElementById('linkedin');
+let email = document.getElementById('email');
+
+email.addEventListener("click", () => {
+    open("https://outlook.live.com/owa/?to=ahickling2004@outlook.com&path=/mail/action/compose")
+})
+
+github.addEventListener("click", () => {
+    open("https://github.com/Ahickling04")
+})
+
+linkedin.addEventListener("click", () => {
+    open("https://www.linkedin.com/in/ahickling04/")
+})
 
 homeButton.addEventListener('click', function() {
     window.location.href = 'index.html';
